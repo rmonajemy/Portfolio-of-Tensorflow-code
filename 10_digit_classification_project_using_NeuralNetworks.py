6522206d0d331ab8f4948aecd8a5ef68209c1601
rmonajemy <rmonajemy@yahoo.com>
2016-11-14 14:09:37 -0800

    Add files via upload

---
+++ 10_digit_classification_project_using_NeuralNetworks.py
@@ -0,0 +1,199 @@
+
+
+
+
+#  10 digit classification project
+
+# used code at this location for reference:
+##  https://github.com/aymericdamien/TensorFlow-Examples
+
+# learn how it works, fine tune, ...  by Ramin Monajemy
+
+
+## 12A  import MNIST data for conv code trial
+
+from __future__ import print_function
+
+import tensorflow as tf
+
+print('IMPORT START 1')
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+print('IMPORT DONE 1')
+
+print('***  download done  ****')
+
+print('MNIST DATA train images: ', mnist.train.images.shape)
+print('MNIST DATA train labels: ', mnist.train.labels.shape)
+print('MNIST DATA validation images: ', mnist.validation.images.shape)
+print('MNIST DATA validation labels: ', mnist.validation.labels.shape)
+print('MNIST DATA test images: ', mnist.test.images.shape)
+print('MNIST DATA test labels: ', mnist.test.labels.shape)
+
+
+
+# In[108]:
+
+
+##12B VERIFIED (9/12/16) Convoulional network example for 10-class mnist digits
+##  https://github.com/aymericdamien/TensorFlow-Examples
+
+
+##########################
+##  SEE https://www.tensorflow.org/versions/0.6.0/tutorials/mnist/pros/index.html 
+## for details of how convoultions work in Tensorflow
+###########################
+
+'''
+A Convolutional Network implementation example using TensorFlow library.
+This example is using the MNIST database of handwritten digits
+(http://yann.lecun.com/exdb/mnist/)
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
+import tensorflow as tf
+
+
+# Parameters
+#learning_rate = 0.001  # ORIGINAL STATEMENT
+Learning_rate = 0.01
+
+#training_iters = 200000
+#training_iters = 5500
+training_iters = 2500
+
+batch_size = 128
+display_step = 10
+
+# Network Parameters
+n_input = 784 # MNIST data input (img shape: 28*28)
+n_classes = 10 # MNIST total classes (0-9 digits)
+dropout = 0.75 # Dropout, probability to keep units
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, n_input])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
+
+
+# Create some wrappers for simplicity
+def conv2d(x, W, b, strides=1):
+    # Conv2D wrapper, with bias and relu activation
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')  # SAME means pad with zeros => output same as input size
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+
+def maxpool2d(x, k=2): # for X = 8 by 8 block of data,  generate a 2x2 where each element is the max of its 2x2 counterpart in X
+    # MaxPool2D wrapper
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
+                          padding='SAME')
+
+
+# Create model
+def conv_net(x, weights, biases, dropout):
+    # Reshape input picture
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+
+    # Convolution Layer
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])    # WC1  = 5, 5, 1, 32 (NOTE:  x,x,1,32 means single layer as input
+                                                        #                            + 32 layers of the first hidden covnet)
+                                                        # bc1  = 32  
+
+    # Max Pooling (down-sampling)
+    conv1 = maxpool2d(conv1, k=2)
+
+    #Convolution Layer
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])  # WC2 = 5, 5, 32, 64  (NOTE:  x,x,32,64 means 32 layers as inputs from above
+                                                          #                            + 64 layers of the second hidden covnet)
+                                                          # BC2 = 64 
+    # Max Pooling (down-sampling)
+    conv2 = maxpool2d(conv2, k=2)  # 
+
+    # CONV2 :  looks like a 7x7,  64 layers deep
+    
+    # Fully connected layer
+    # Reshape conv2 output to fit fully connected layer input
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])  # reshaper to a (1)x(7x7x64) data vector    
+    
+    print("TEST",[-1, weights['wd1'].get_shape().as_list()[0]])
+    
+    # CONV2 :  looks like a 7x7,  64 layers deep
+    
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])  # WD1 = (1, 1024) data vector
+                                                                 # BD1 = 1024
+
+    fc1 = tf.nn.relu(fc1)               #WORKING BACKWARDS:     fc1 = 1x1024
+    # Apply Dropout
+ 
+    fc1 = tf.nn.dropout(fc1, dropout)   # do drop out with probability of 0.75
+                                        # With probability keep_prob, outputs the input element scaled up by 1 / keep_prob, 
+                                        # otherwise outputs 0. The scaling is so that the expected sum is unchanged.
+
+    # Output, class prediction
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])  # out is 1024x1 data vector X W_out = 1x10
+    
+    return out
+
+# Store layers weight & bias
+weights = {
+    # 5x5 conv, 1 input, 32 outputs
+    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    # 5x5 conv, 32 inputs, 64 outputs
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    # fully connected, 7*7*64 inputs, 1024 outputs
+    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+    # 1024 inputs, 10 outputs (class prediction)
+    'out': tf.Variable(tf.random_normal([1024, n_classes]))
+}
+
+biases = {
+    'bc1': tf.Variable(tf.random_normal([32])),
+    'bc2': tf.Variable(tf.random_normal([64])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+# Construct model
+pred = conv_net(x, weights, biases, keep_prob)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.AdamOptimizer(learning_rate=Learning_rate).minimize(cost)
+
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initializing the variables
+init = tf.initialize_all_variables()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+    # Keep training until reach max iterations
+    while step * batch_size < training_iters:
+        batch_x, batch_y = mnist.train.next_batch(batch_size)   # batch size is 128
+        # Run optimization op (backprop)
+        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y,
+                                       keep_prob: dropout})
+        if step % display_step == 0:
+            # Calculate batch loss and accuracy
+            loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x,
+                                                              y: batch_y,
+                                                              keep_prob: 1.})
+            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " +                   "{:.6f}".format(loss) + ", Training Accuracy= " +                   "{:.5f}".format(acc))
+        step += 1
+    print("Optimization Finished!")
+
+    # Calculate accuracy for 256 mnist test images
+    print("Testing Accuracy:",         sess.run(accuracy, feed_dict={x: mnist.test.images[:256],
+                                      y: mnist.test.labels[:256],
+                                      keep_prob: 1.}))
---
+++ Edge_Detector_using_TensorFlow.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 14:00:14 2016
+
+@author: Ramin
+"""
+
+# In[11]:
+
+
+#  Edge Detector in Tensorflow
+
+#1-  peripehry
+#2-  horizonal edges
+#3-  vertical edges
+
+#Author:  Ramin Monajemy
+
+
+
+# USE 16A to import data
+import tensorflow as tf
+import numpy as np
+
+
+#print a sample first
+
+import matplotlib.pyplot as plt
+get_ipython().magic(u'matplotlib inline')
+
+
+from __future__ import print_function
+
+#image = plt.imread("bridge.jpg")  
+image = plt.imread("parrot.png")    # parrt.png is 360x412x4
+#image = plt.imread("ID-100137333.jpg")
+
+
+
+
+#imgplot = plt.imshow(image)
+print(image.shape)
+M = image.shape[0]
+N = image.shape[1]
+P1 = np.zeros([M,N])
+for i in range(M):
+      for j in range(N):
+        P1[i][j] = image[i][j][0]  # CHOOSE LEVEL 0 OF 4 LAYERS THAT DEFINE COLOR AND ATTRIBUTES OF IMAGE
+           
+
+# TEST MY EDGE DETECTOR USING A SIMPLE MATRIX            
+MAT = np.array(  [ [0,  0,  0,  0, 0, 0,  0,  0,  0, 0, 0,  0,  0,  0, 0], 
+                   [0,  0,  0,  0, 0, 0,  0,  0,  0, 0, 0,  0,  0,  0, 0], 
+                   [0,  0,  0,  0, 0, 0,  0,  0,  0, 0, 0,  0,  0,  0, 0],             
+                   [0,  0,  0,  100, 100, 100,  100,  100,  100, 100, 0,  0,  0,  0, 0], 
+                   [0,  0,  0,  100, 100, 100,  100,  100,  100, 100, 0,  0,  0,  0, 0], 
+                   [0,  0,  0,  100, 100, 100,  100,  100,  100, 100, 0,  0,  0,  0, 0], 
+                   [0,  0,  0,  100, 100, 100,  100,  100,  100, 100, 0,  0,  0,  0, 0], 
+                   [0,  0,  0,  100, 100, 100,  100,  100,  100, 100, 0,  0,  0,  0, 0],
+                   [0,  0,  0,  0, 0, 0,  0,  0,  0, 0,  0,  0,  0,  0, 0], 
+                   [0,  0,  0,  0, 0, 0,  0,  0,  0, 0,  0,  0,  0,  0, 0]])
+            
+
+    
+    
+    
+# NOW PREPARE the variable for THE TENSOR FLOW
+
+MATRIX_or_IMAGE = 0  #  0 for simple matrix, 1 for image
+if (MATRIX_or_IMAGE == 1):
+    MAT = P1     # USE THIS TO PROCESS PARROT OR BRIDGE IMAGE
+else:
+
+    #P1 = MAT   # USE MATRIX AS INPUT COMMENT OUT THESE FOUR LINES TO USE ONE OF THE IMAGES (PARROT OR BRIDGE) INSTEAD IF SIMPLE MATRIX
+    #x = MAT
+    M = 10
+    N = 15
+
+
+""""
+x = tf.convert_to_tensor(x,dtype=tf.float32)  # convert integer array to float32 tensor  (NOTE: (A) CONVERT TO TENSOR  (B) RECAST AS FLOAT32)
+print('convert inout to float32 tensor: ', x)
+
+#x = tf.reshape(x, shape=[-1, 1,M*N, 1])             #CONV needs a 4-dim (rank 4) input  DONT NEED THIS
+#print(x)
+x = tf.reshape(x, shape=[-1, M, N, 1])               # TENSORFLOW CONV COMMANDS NEED RANK OF TENSOR TO BE 4 (4 DIMENTIOANL TENSOR)
+#  -1 is inferred such that total number of emelements does not change
+print('convert tensor to rank 4 (4-dimentional tensor) for proper input to conv() command: ', x)
+
+# THIS STATEMENT NEEDED FOR PROPER IMAGE PROCESSING FOR TENSOR FLOW FOR IMAGES,  BUT WHY? 
+#x = tf.convert_to_tensor(x,dtype=tf.float64)              # could not convert using float32  REMOVE JUST FOR 10x10 example
+
+
+#x = tf.cast(x, tf.float32)  # tensor flow code needs tensors in float32  (needed if origianl variable is FLOAT64 LIKE SOME IMAGES)
+"""
+
+# USE feed_dict to transfer data to sessin
+
+#1- input data remains a numpy array
+#2- but, its reshaped to have rank 4 as needed for TensorFlow CONV
+
+#MAT = MAT.astype("float32")  # make sure data type is float32 as needed in TensorFlow
+
+DATA = MAT.reshape(1,M,N,1)  # no longer a tensor
+
+#3-  need to define x as a tensor
+x = tf.placeholder("float32")
+
+
+#1  define parameters and filter for proper input into conv2d
+
+#######################################################################
+#KERNEL 1  edge detect
+FILTER = np.random.random_sample((5,5,1,1))   # filter input into conv2d:  rows, columns, input layers, output layers
+
+KK = np.array(    [[0.,  0.,  0.,  0., 0.], 
+                   [0.,  0.,  1.,  0., 0.],
+                   [0.,  1., -4.,  1., 0.],
+                   [0.,  0.,  1.,  0., 0.],
+                   [0.,  0.,  0.,  0., 0.]])
+KK = KK*5
+
+for i in range(len(KK)):
+    for j in range(len(KK)):
+        FILTER[i][j][0][0] = KK[i][j]  # prepare the Kenel to meet TensorFlow convolusion command requirements  [data][data], [input layesr],[output la]
+        
+KERNEL1 = tf.constant(FILTER, dtype=tf.float32)
+
+#######################################################################
+#KERNEL 2  trial
+
+FILTER = np.random.random_sample((5,5,1,1))   # SHARPER IMAGE filter input into conv2d:  rows, columns, input layers, output layers
+KK = np.array(    [[0.,  -1.,  1.,  0., 0.], 
+                   [0.,  -1.,  1.,  0., 0.],
+                   [0.,  -1.,  1.,  0., 0.],
+                   [0.,  -1.,  1.,  0., 0.],
+                   [0.,  -1.,  1.,  0., 0.]])
+               
+KK = KK*10    
+    
+for i in range(len(KK)):
+    for j in range(len(KK)):
+        FILTER[i][j][0][0] = KK[i][j]
+
+KERNEL2 = tf.constant(FILTER, dtype=tf.float32)
+
+
+#######################################################################
+#KERNEL 3  trial
+
+FILTER = np.random.random_sample((5,5,1,1))   # SHARPER IMAGE filter input into conv2d:  rows, columns, input layers, output layers
+factor = .1
+KK = np.array(    [[0.,    0.,   0.,  0.,  0.], 
+                   [0.,    0.,   0.,  0.,  0.],
+                   [-factor,  -factor,  -factor, -factor, -factor],
+                   [ factor,   factor,   factor,  factor,  factor],
+                   [0.,    0.,   0.,  0.,  0.]])
+               
+for i in range(len(KK)):
+    for j in range(len(KK)):
+        FILTER[i][j][0][0] = KK[i][j]
+
+KERNEL3 = tf.constant(FILTER, dtype=tf.float32)
+
+
+
+
+
+strides = 1
+
+
+#2   Construct model
+x_out1 = tf.nn.conv2d(x, KERNEL1, strides=[1, strides, strides, 1], padding='SAME')  # SAME means pad with zeros => output same as input size
+x_out2 = tf.nn.conv2d(x, KERNEL2, strides=[1, strides, strides, 1], padding='SAME')  # SAME means pad with zeros => output same as input size
+
+x_out3 = tf.nn.conv2d(x, KERNEL3, strides=[1, strides, strides, 1], padding='SAME')  # SAME means pad with zeros => output same as input size
+
+
+#3  Initializing the variables
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init)
+    
+    out1 = sess.run(x_out1, feed_dict={x: DATA})
+    out2 = sess.run(x_out2, feed_dict={x: DATA})
+    out3 = sess.run(x_out3, feed_dict={x: DATA})
+    
+    
+    FIG1 = np.zeros([M,N])
+    FIG2 = np.zeros([M,N])
+    FIG3 = np.zeros([M,N])
+    for i in range(0,M):
+      for j in range(0,N):
+        FIG1[i][j] = out1[0][i][j][0]
+        FIG2[i][j] = out2[0][i][j][0]
+        FIG3[i][j] = out3[0][i][j][0]
+        
+    _, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4)
+    
+
+    #ax1.imshow(x1_test.reshape(28, 28), cmap=plt.cm.Greys, vmin = 0, vmax = 1); # cmap=plt.cm.Greys sees min(x) white, max(x) black
+    #ax1.set_ylabel("y: 28 point")
+    #ax1.set_xlabel("x: 28 point")
+    #ax1.set_title("input figure")
+    
+    
+    ax1.imshow(MAT, cmap=plt.cm.Greys, vmin = 0, vmax = 1);
+    ax1.set_title("Level 0 ")
+    
+    ax2.imshow(FIG1, cmap=plt.cm.Greys, vmin = -1, vmax = 1);
+    ax2.set_title("FILTER 1")
+    
+    ax3.imshow(FIG2, cmap=plt.cm.Greys, vmin = -1, vmax = 1);
+    ax3.set_title("FILTER 2")
+    
+    ax4.imshow(FIG3, cmap=plt.cm.Greys, vmin = -1, vmax = 1);
+    ax4.set_title("FILTER 3")
+    
+    """
+    ax1.imshow(P1, cmap=plt.cm.Greys, vmin = 0, vmax = 250);
+    ax1.set_title("Level 0 ")
+    
+    ax2.imshow(FIG1, cmap=plt.cm.Greys, vmin = -250, vmax = 250);
+    ax2.set_title("FILTER 1")
+    
+    ax3.imshow(FIG2, cmap=plt.cm.Greys, vmin = -250, vmax = 250);
+    ax3.set_title("FILTER 2")
+    
+    ax4.imshow(FIG3, cmap=plt.cm.Greys, vmin = -250, vmax = 250);
+    ax4.set_title("FILTER 3")
+    """
+    
+    
